dda75fa52a9b345810d5e3cecef75958e89968c4
facumontes <facundomontes2204@gmail.com>
2025-12-11 09:46:10 -0300

    add hola mundo with Rust language

---
+++ hola-mundo/target/debug/deps/hola_mundo.d
@@ -0,0 +1,5 @@
+E:\proyectos\learning-github-actions\hola-mundo\target\debug\deps\hola_mundo.d: src\main.rs
+
+E:\proyectos\learning-github-actions\hola-mundo\target\debug\deps\hola_mundo.exe: src\main.rs
+
+src\main.rs:
---
+++ hola-mundo/target/debug/hola-mundo.d
@@ -0,0 +1,1 @@
+E:\proyectos\learning-github-actions\hola-mundo\target\debug\hola-mundo.exe: E:\proyectos\learning-github-actions\hola-mundo\src\main.rs
